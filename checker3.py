fb3dfc6331b952f93ca18cafd89a28c1d55b0cdb
1dot75cm <sensor.wen@gmail.com>
2016-01-24 23:42:19 +0800

    dump version 0.3
    Refactoring for checker3

--- checker3.py
+++ checker3.py
@@ -6,8 +6,9 @@
 @project: Checker
 @github: http://github.com/1dot75cm/repo-checker
 @description: Checker is used to check update for software.
-@version: 0.2
+@version: 0.3
 @history:
+    0.3 - use class (2016.01.24)
     0.2 - Add help info, use multithread (2016.01.10)
     0.1 - Initial version (2016.01.05)
 '''
@@ -15,268 +16,263 @@
 from threading  import Thread
 from queue      import Queue
 import urllib.request, urllib.parse, urllib.error
-import re
-import time
-import csv
-import json
-import sys
-import os
+import re, csv, json
+import time, sys, os
 import argparse
 
-def load_data(file):
-    ''' Load csv file.
+class Checker(object):
+    ''' check update for software '''
 
-    CSV Format:
-      "1,1,lwqq,https://github.com/xiehuc/lwqq,master,733836e,150202"
+    def __init__(self, *args):
+        self.sub_pkg, self.url_type, self.name, self.url = args[:4]
+        self.branch, self.rpm_com, self.rpm_date = args[4:7]
+        self.release_date = self.release_com = ""
+        self.latest_date = self.latest_com = self.status = ""
 
-    Sub Package: 0. normal 1. subpkg
-    Url Type: 1. github 2. google 3. bitbucket 4. sogou 5. tragtor 6. youdao
-      7. opera 8. pacman 9. apt 10. nginx 11. ccal 12. sandbox 14. wps
-      20. qt-installer-freamwork 21. pycharm
-    Name, Url, Branch, RPM Commit, RPM Update Time, Others
+    def get_page(self, _url):
+        ''' 获取整个页面数据
+        return str '''
 
-    return list '''
-
-    csvlist = list()
-    with open(file, 'r') as csvfile:
-        content = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in content:
-            csvlist.append(row)
-    return csvlist
-
-def get_page(url, url_type):
-    ''' 获取整个页面数据
-    return str '''
-
-    if url_type == "2":
-        return "None Content"
-    elif url_type == "4":
-        return urllib.request.urlopen(url).read().decode('gb2312').encode('utf-8')
-    else:
-        return urllib.request.urlopen(url).read()
-
-def get_date(page, url_type):
-    ''' 获取 date -> 150708
-    return str '''
-
-    time_format = "%Y-%m-%d"
-    pattern = b'.*datetime="(.*?)T'
-
-    if url_type in ["4", "6", "14"]:
-        pattern = b'([0-9]{4}-[0-9]{1,2}-[0-9]{1,2})'
-    elif url_type == "5":
-        page = page.split(b"tragtor")[4]
-        pattern = b'time.>(.*[0-9]{4})'
-        time_format = "%a %b %d %H:%M:%S %Y"
-    elif url_type == "7":
-        if page.splitlines()[-3].find(b"info") > -1:
-            page = page.splitlines()[-4]
+        if self.url_type == "2":
+            return "None Content"
+        elif self.url_type == "4":
+            return urllib.request.urlopen(_url).read().decode('gb2312').encode('utf-8')
         else:
-            page = page.splitlines()[-3]
-        pattern = b'([0-9]{2}-.{3}-[0-9]{4})'
-        time_format = "%d-%b-%Y"
-    elif url_type == "8":
-        pattern = b'tag.*([0-9]{4}-[0-9]{1,2}-[0-9]{1,2})'
-    elif url_type == "9":
-        pattern = b'id=\w{40}.*([0-9]{4}-[0-9]{2}-[0-9]{2})'
-    elif url_type == "10":
-        pattern = b'([0-9]{2}\s.{3}\s[0-9]{4})'
-        time_format = "%d %b %Y"
-    elif url_type == "11":
-        pattern = b'([A-Z].*\s[0-9]{1,2},\s[0-9]{4})'
-        time_format = "%B %d, %Y"
-    elif url_type == "12":
-        pattern = b'id=\w{40}.*tag-deco.*([0-9]{4}-[0-9]{2}-[0-9]{2})'
-    elif url_type == "20":
-        pattern = b'([0-9]{2}-.{3}-[0-9]{4})'
-        time_format = "%d-%b-%Y"
-    elif url_type == "21":
-        page = json.loads(page.decode())['PCC'][0]['date'].encode()
-        pattern = b'(.*)'
+            return urllib.request.urlopen(_url).read()
 
-    try:
-        d = re.search(pattern, page, re.M).group(1).decode()
-        t = time.strptime(d, time_format)
-        short_time = time.strftime("%y%m%d", t)
-    except (AttributeError, TypeError):
-        short_time = "None"
+    def get_date(self, _page):
+        ''' 获取 date -> 150708
+        return short_time(str) '''
 
-    return short_time
+        time_format = "%Y-%m-%d"
+        pattern = b'.*datetime="(.*?)T'
 
-def get_commit(page, url_type):
-    ''' 获取 commit
-    return str '''
+        if self.url_type in ["4", "6", "14"]:
+            pattern = b'([0-9]{4}-[0-9]{1,2}-[0-9]{1,2})'
+        elif self.url_type == "5":
+            _page = _page.split(b"tragtor")[4]
+            pattern = b'time.>(.*[0-9]{4})'
+            time_format = "%a %b %d %H:%M:%S %Y"
+        elif self.url_type == "7":
+            if _page.splitlines()[-3].find(b"info") > -1:
+                _page = _page.splitlines()[-4]
+            else:
+                _page = _page.splitlines()[-3]
+            pattern = b'([0-9]{2}-.{3}-[0-9]{4})'
+            time_format = "%d-%b-%Y"
+        elif self.url_type == "8":
+            pattern = b'tag.*([0-9]{4}-[0-9]{1,2}-[0-9]{1,2})'
+        elif self.url_type == "9":
+            pattern = b'id=\w{40}.*([0-9]{4}-[0-9]{2}-[0-9]{2})'
+        elif self.url_type == "10":
+            pattern = b'([0-9]{2}\s.{3}\s[0-9]{4})'
+            time_format = "%d %b %Y"
+        elif self.url_type == "11":
+            pattern = b'([A-Z].*\s[0-9]{1,2},\s[0-9]{4})'
+            time_format = "%B %d, %Y"
+        elif self.url_type == "12":
+            pattern = b'id=\w{40}.*tag-deco.*([0-9]{4}-[0-9]{2}-[0-9]{2})'
+        elif self.url_type == "20":
+            pattern = b'([0-9]{2}-.{3}-[0-9]{4})'
+            time_format = "%d-%b-%Y"
+        elif self.url_type == "21":
+            _page = json.loads(_page.decode())['PCC'][0]['date'].encode()
+            pattern = b'(.*)'
 
-    pattern = b'.*commit/([a-z0-9]*)\"'
+        try:
+            d = re.search(pattern, _page, re.M).group(1).decode()
+            t = time.strptime(d, time_format)
+            return time.strftime("%y%m%d", t)
+        except (AttributeError, TypeError):
+            return "None"
 
-    if url_type == "9":
-        pattern = b'id=(\w{40})'
-    elif url_type == "12":
-        pattern = b'id=(\w{40}).*tag-deco'
+    def get_commit(self, _page):
+        ''' 获取 commit
+        return commit(str) '''
 
-    try:
-        c = re.search(pattern, page, re.M).group(1).decode()
-    except (AttributeError, TypeError):
-        c = "None"
+        pattern = b'.*commit/([a-z0-9]*)\"'
 
-    return c
+        if self.url_type == "9":
+            pattern = b'id=(\w{40})'
+        elif self.url_type == "12":
+            pattern = b'id=(\w{40}).*tag-deco'
 
-def get_info(*item):
-    ''' 获取页面commit和date
-    return datalist'''
+        try:
+            return re.search(pattern, _page, re.M).group(1).decode()
+        except (AttributeError, TypeError):
+            return "None"
 
-    item = item[0]
-    if len(item) == 0 or item[0][0] == "#":
-        return None
+    def get_info(self):
+        ''' 获取页面commit和date
+        return datalist'''
 
-    sub_pkg, url_type, name, url = item[0], item[1], item[2], item[3]
-    branch, rpm_com, rpm_date = item[4], item[5], item[6]
+        type_num = ["3", "4", "5", "6", "7", "9", "10", "11", "12", "14", "20", "21"]
 
-    type_num = ["3", "4", "5", "6", "7", "9", "10", "11", "12", "14", "20", "21"]
-
-    if url_type in type_num:
-        release_url, latest_url = url.replace('%2',','), url.replace('%2',',')
-    else:
-        release_url = url + "/releases"
-        latest_url = url + "/commits/" + branch
-
-    page = get_page(release_url, url_type)
-    release_date = get_date(page, url_type)
-    release_com = get_commit(page, url_type)
-
-    page = get_page(latest_url, url_type)
-    latest_date = get_date(page, url_type)
-    latest_com = get_commit(page, url_type)
-
-    if rpm_date == latest_date or rpm_date >= release_date:
-        status = "normal"
-    else:
-        status = "update" + "[" + url + "]"
-
-    itemlist = [sub_pkg, name, rpm_date, rpm_com, release_date, release_com, latest_date, latest_com, status]
-
-    return output(0, *itemlist)
-
-def output(title=0, *datalist):
-    ''' Output.
-    Sub Package: 0. normal 1. subpkg '''
-
-    if title == 1:
-        print("# Generation date:", localtime(1), \
-              "\n Name".ljust(23) + \
-              "RPM-Version".ljust(16) + \
-              "Rel-Version".ljust(16) + \
-              "Latest-Commit".ljust(16) + \
-              "Status")
-    else:
-        if datalist[0] == "1":
-            name = "- " + datalist[1]
+        if self.url_type in type_num:
+            release_url, latest_url = self.url.replace('%2',','), self.url.replace('%2',',')
         else:
-            name = datalist[1]
+            release_url = self.url + "/releases"
+            latest_url = self.url + "/commits/" + self.branch
 
-        rpm_ver = datalist[2] + "[" + datalist[3][:7] + "]"
-        rel_ver = datalist[4] + "[" + datalist[5][:7] + "]"
-        lat_ver = datalist[6] + "[" + datalist[7][:7] + "]"
+        p1, p2 = self.get_page(release_url), self.get_page(latest_url)
+        self.release_date, self.release_com = self.get_date(p1), self.get_commit(p1)
+        self.latest_date, self.latest_com = self.get_date(p2), self.get_commit(p2)
+
+        if self.rpm_date == self.latest_date or self.rpm_date >= self.release_date:
+            self.status = "normal"
+        else:
+            self.status = "update" + "[" + self.url + "]"
+
+    def output(self):
+        ''' Output.
+        Sub Package: 0. normal 1. subpkg '''
+
+        name = "- " + self.name if self.sub_pkg == "1" else self.name
+        rpm_ver = self.rpm_date + "[" + self.rpm_com[:7] + "]"
+        rel_ver = self.release_date + "[" + self.release_com[:7] + "]"
+        lat_ver = self.latest_date + "[" + self.latest_com[:7] + "]"
         print(str(name).ljust(22) + \
               str(rpm_ver).ljust(16) + \
               str(rel_ver).ljust(16) + \
               str(lat_ver).ljust(16) + \
-              str(datalist[8]))
+              str(self.status))
 
-def localtime(trigger=1):
-    ''' return current localtime and seconds. '''
+class Helper(object):
 
-    global start_sec, stop_sec
-    if trigger == 1:
-        start_sec = time.time()
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    elif trigger == 0:
-        stop_sec = time.time()
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), stop_sec - start_sec
+    def __init__(self):
+        self.thread_num = 10
+        self.data_file = 'checker_data.csv'
+        self.q = Queue()
 
-def helper():
-    ''' display help information.
-    return csv_path, thread_num'''
+    def load_data(self):
+        ''' Load csv file.
 
-    thread_num = 10
-    data_file = 'checker_data.csv'
-    doclist = list()
+        CSV Format:
+          "1,1,lwqq,https://github.com/xiehuc/lwqq,master,733836e,150202"
 
-    for i in __doc__.splitlines():
-        if i.startswith("@") and i.find(": ") > -1:
-            doclist.append(i.split(': ')[1])
-    _author, _email, _project, _github, _description, _version = doclist
+        Sub Package: 0. normal 1. subpkg
+        Url Type: 1. github 2. google 3. bitbucket 4. sogou 5. tragtor 6. youdao
+          7. opera 8. pacman 9. apt 10. nginx 11. ccal 12. sandbox 14. wps
+          20. qt-installer-freamwork 21. pycharm
+        Name, Url, Branch, RPM Commit, RPM Update Time, Others
 
-    parser = argparse.ArgumentParser(description=_description)
+        return list '''
 
-    parser.add_argument('-n', '--number', metavar='NUM', type=int,
-                        dest='numbers', action='store',
-                        help='{_name} work number of thread(default: 10)'.format(
-                            _name=_project.lower())
-                       )
+        csvlist = []
+        with open(self.data_file, 'r') as csvfile:
+            content = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in content:
+                if len(row) != 0 and row[0][0] != "#":
+                    csvlist.append(row)
+        return csvlist
 
-    parser.add_argument('-f', '--file', metavar='PATH',
-                        dest='files', action='store',
-                        help='{_name} data(csv) full path'.format(
-                            _name=_project.lower())
-                       )
+    def output(self, title=None):
+        ''' Output. '''
 
-    parser.add_argument('-v', '--version', dest='version', action='store_true',
-                        help='output version information and exit')
+        if title == 1:
+            print("# Generation date:", self.localtime(1), \
+                  "\n Name".ljust(23) + \
+                  "RPM-Version".ljust(16) + \
+                  "Rel-Version".ljust(16) + \
+                  "Latest-Commit".ljust(16) + \
+                  "Status")
+        else:
+            print("Bye~ %s  Working: %s Sec" % self.localtime(0))
 
-    args = parser.parse_args()
+    def localtime(self, trigger=1):
+        ''' return current localtime and seconds. '''
 
-    if args.version:
-        print('{} version {}\nWritten by {} <{}>\nReport bug: <{}>'.format(
-                 _project, _version, _author, _email, _github)
-             )
-        sys.exit()
+        global start_sec, stop_sec
+        if trigger == 1:
+            start_sec = time.time()
+            return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        elif trigger == 0:
+            stop_sec = time.time()
+            return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), stop_sec - start_sec
 
-    if args.files and os.path.exists(args.files):
-        data_file = args.files
-    elif args.files is not None:
-        print("{}: cannot access '{}': No such file or directory"
-            .format(_project, args.files))
-        sys.exit()
+    def helper(self):
+        ''' display help information.
+        return csv_path, thread_num'''
 
-    if args.numbers:
-        thread_num = args.numbers
+        doclist = []
+        for i in __doc__.splitlines():
+            if i.startswith("@") and i.find(": ") > -1:
+                doclist.append(i.split(': ')[1])
+        _author, _email, _project, _github, _description, _version = doclist
 
-    return data_file, thread_num
+        parser = argparse.ArgumentParser(description=_description)
 
-def working(q):
-    ''' get content from queue. '''
+        parser.add_argument('-n', '--number', metavar='NUM', type=int,
+                            dest='numbers', action='store',
+                            help='{_name} work number of thread(default: 10)'.format(
+                                _name=_project.lower())
+                           )
 
-    while True:
-        value = [q.get(),]
-        list(filter(get_info, value))
-        q.task_done()
+        parser.add_argument('-f', '--file', metavar='PATH',
+                            dest='files', action='store',
+                            help='{_name} data(csv) full path'.format(
+                                _name=_project.lower())
+                           )
 
-def running(Num=10, *data):
-    ''' Mutilthreads test: 1, 569s; 5, 107s; 10, 54s; 15, 37s
-    create threads, and put data to queue. '''
+        parser.add_argument('-v', '--version', dest='version', action='store_true',
+                            help='output version information and exit')
 
-    q = Queue()
+        args = parser.parse_args()
 
-    for i in range(Num):
-        t = Thread(target=working, args=(q,))
-        t.setDaemon(True)
-        t.start()
+        if args.version:
+            print('{} version {}\nWritten by {} <{}>\nReport bug: <{}>'.format(
+                     _project, _version, _author, _email, _github)
+                 )
+            sys.exit()
 
-    for value in data:
-        q.put(value)
+        if args.files and os.path.exists(args.files):
+            self.data_file = args.files
+        elif args.files is not None:
+            print("{}: cannot access '{}': No such file or directory"
+                .format(_project, args.files))
+            sys.exit()
 
-    q.join()
+        if args.numbers:
+            self.thread_num = args.numbers
 
+    def working(self):
+        ''' get content from queue. '''
+
+        while True:
+            obj = self.q.get()
+            obj.get_info()
+            self.q.task_done()
+
+    def running(self, *args):
+        ''' Mutilthreads test: 1, 569s; 5, 107s; 10, 54s; 15, 37s
+        create threads, and put data to queue. '''
+
+        for i in range(self.thread_num):
+            t = Thread(target=self.working, args=())
+            t.setDaemon(True)
+            t.start()
+
+        for item in args:
+            self.q.put(item)
+
+        self.q.join()
 
 if __name__ == "__main__":
-    csv_path, thread_num = helper()
-    data = load_data(csv_path)
-    output(title=1)
+    tools = Helper()
+    tools.helper()
+    tools.output(1)
+
+    objlist = []
+    for i in tools.load_data():
+        i[2] = Checker(*i)
+        objlist.append(i[2])
 
     try:
-        running(thread_num, *data)
+        tools.running(*objlist)
     except KeyboardInterrupt:
         pass
 
-    print("Bye~ %s  Working: %s Sec" % localtime(0))
+    for obj in objlist:
+        obj.output()
+
+    tools.output()
