237b799b887b4221ad3afb8d2b0688a269727b40
1dot75cm <sensor.wen@gmail.com>
2017-01-16 08:58:56 +0800

    update zh_CN translation
    Signed-off-by: 1dot75cm <sensor.wen@gmail.com>

--- locale/zh_CN.ts
+++ locale/zh_CN.ts
@@ -3,219 +3,370 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../checker/gui.py" line="392"/>
+        <location filename="../checker/frontends/gui.py" line="417"/>
         <source>Checker</source>
         <translation>检查器</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="141"/>
+        <location filename="../checker/frontends/gui.py" line="142"/>
         <source>Check</source>
         <translation>检查</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="145"/>
+        <location filename="../checker/frontends/gui.py" line="146"/>
         <source>Update item</source>
         <translation>更新项</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="375"/>
+        <location filename="../checker/frontends/gui.py" line="395"/>
         <source>Edit rule</source>
         <translation>编辑规则</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="158"/>
+        <location filename="../checker/frontends/gui.py" line="162"/>
         <source>File</source>
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="160"/>
+        <location filename="../checker/frontends/gui.py" line="170"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="168"/>
+        <location filename="../checker/frontends/gui.py" line="178"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="171"/>
+        <location filename="../checker/frontends/gui.py" line="181"/>
         <source>About Qt</source>
         <translation>关于 Qt</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="174"/>
+        <location filename="../checker/frontends/gui.py" line="184"/>
         <source>&amp;Open</source>
         <translation>打开(&amp;O)</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="176"/>
+        <location filename="../checker/frontends/gui.py" line="186"/>
         <source>Open a file</source>
         <translation>打开一个文件</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="179"/>
+        <location filename="../checker/frontends/gui.py" line="193"/>
         <source>&amp;Save</source>
         <translation>保存(&amp;S)</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="181"/>
+        <location filename="../checker/frontends/gui.py" line="195"/>
         <source>Save a file</source>
         <translation>保存一个文件</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="184"/>
+        <location filename="../checker/frontends/gui.py" line="198"/>
         <source>Save As</source>
         <translation>另存为</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="187"/>
+        <location filename="../checker/frontends/gui.py" line="201"/>
         <source>&amp;Close</source>
         <translation>关闭(&amp;C)</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="189"/>
+        <location filename="../checker/frontends/gui.py" line="203"/>
         <source>Close current page</source>
         <translation>关闭当前页</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="191"/>
+        <location filename="../checker/frontends/gui.py" line="205"/>
         <source>&amp;Exit</source>
         <translation>退出(&amp;E)</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="193"/>
+        <location filename="../checker/frontends/gui.py" line="207"/>
         <source>Exit application</source>
         <translation>退出应用程序</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="228"/>
+        <location filename="../checker/frontends/gui.py" line="248"/>
         <source>Message</source>
         <translation>消息</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="228"/>
+        <location filename="../checker/frontends/gui.py" line="248"/>
         <source>Are you sure to quit?</source>
         <translation>您确定要退出吗？</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="385"/>
+        <location filename="../checker/frontends/gui.py" line="405"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="265"/>
+        <location filename="../checker/frontends/gui.py" line="285"/>
         <source>The row is to top.</source>
         <translation>行已到顶部。</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="267"/>
+        <location filename="../checker/frontends/gui.py" line="287"/>
         <source>The row is to bottom.</source>
         <translation>行已到底部。</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="385"/>
+        <location filename="../checker/frontends/gui.py" line="405"/>
         <source>Please select a row.</source>
         <translation>请选择一行。</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="315"/>
+        <location filename="../checker/frontends/gui.py" line="335"/>
         <source>checking...</source>
         <translation>正在检查...</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="353"/>
+        <location filename="../checker/frontends/gui.py" line="373"/>
         <source>finished (work time %ds)</source>
         <translation>完成 (耗时 %ds)</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="383"/>
+        <location filename="../checker/frontends/gui.py" line="403"/>
         <source>The row is empty.</source>
         <translation>该行是空行。</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="375"/>
+        <location filename="../checker/frontends/gui.py" line="395"/>
         <source>XPath rule(format: &quot;[(time, commit), (time, commit)]&quot;):</source>
         <translation>XPath 规则 (格式: &quot;[(时间, 提交), (时间, 提交)]&quot;)</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="407"/>
+        <location filename="../checker/frontends/gui.py" line="446"/>
         <source>Open file</source>
         <translation>打开文件</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="433"/>
+        <location filename="../checker/frontends/gui.py" line="477"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="413"/>
-        <source>Open file failed.</source>
-        <translation>打开文件失败。</translation>
-    </message>
-    <message>
-        <location filename="../checker/gui.py" line="418"/>
-        <source>The file does not contain JSON or CSV.</source>
-        <translation>该文件不包含 JSON 或 CSV。</translation>
-    </message>
-    <message>
-        <location filename="../checker/gui.py" line="422"/>
-        <source>open successfully</source>
-        <translation>打开成功</translation>
-    </message>
-    <message>
-        <location filename="../checker/gui.py" line="425"/>
+        <location filename="../checker/frontends/gui.py" line="467"/>
         <source>Save file</source>
         <translation>保存文件</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="431"/>
+        <location filename="../checker/frontends/gui.py" line="475"/>
         <source>saved successfully</source>
         <translation>保存成功</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="433"/>
-        <source>Save file failed.</source>
-        <translation>保存文件失败。</translation>
-    </message>
-    <message>
-        <location filename="../checker/gui.py" line="80"/>
+        <location filename="../checker/frontends/gui.py" line="81"/>
         <source>Name</source>
         <translation>项目名</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="80"/>
+        <location filename="../checker/frontends/gui.py" line="81"/>
         <source>URL</source>
         <translation>链接</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="80"/>
+        <location filename="../checker/frontends/gui.py" line="81"/>
         <source>Branch</source>
         <translation>分支</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="81"/>
+        <location filename="../checker/frontends/gui.py" line="82"/>
         <source>RPM date [commit]</source>
         <translation>RPM 日期 [提交]</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="81"/>
+        <location filename="../checker/frontends/gui.py" line="82"/>
         <source>Release date [commit]</source>
         <translation>发布日期 [提交]</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="82"/>
+        <location filename="../checker/frontends/gui.py" line="83"/>
         <source>Latest date [commit]</source>
         <translation>最新日期 [提交]</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="82"/>
+        <location filename="../checker/frontends/gui.py" line="83"/>
         <source>Status</source>
         <translation>状态</translation>
     </message>
     <message>
-        <location filename="../checker/gui.py" line="82"/>
+        <location filename="../checker/frontends/gui.py" line="83"/>
         <source>Comment</source>
         <translation>注释</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="166"/>
+        <source>Tool</source>
+        <translation>工具</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="209"/>
+        <source>&amp;Settings</source>
+        <translation>设置(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="211"/>
+        <source>Open settings dialog</source>
+        <translation>打开设置对话框</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="189"/>
+        <source>Open &amp;url</source>
+        <translation>打开 URL(&amp;U)</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="191"/>
+        <source>Open a file with url</source>
+        <translation>从链接打开一个文件</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="421"/>
+        <source>Open url</source>
+        <translation>打开 URL</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="421"/>
+        <source>Enter url:</source>
+        <translation>输入 URL：</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="427"/>
+        <source>open url successfully</source>
+        <translation>打开 URL 成功</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="431"/>
+        <source>open url failed</source>
+        <translation>打开 URL 失败</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="454"/>
+        <source>Open file failed. See below:
+%s</source>
+        <translation>打开文件失败。错误信息如下：
+%s</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="460"/>
+        <source>The file does not contain JSON or CSV. See below:
+%s</source>
+        <translation>该文件不包含 JSON 或 CSV。错误信息如下：
+%s</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="464"/>
+        <source>open file successfully</source>
+        <translation>打开文件成功</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="477"/>
+        <source>Save file failed. See below:
+%s</source>
+        <translation>保存文件失败。错误信息如下：
+%s</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="429"/>
+        <source>Open url failed. See below:
+%s</source>
+        <translation>打开文件失败。错误信息如下：
+%s</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/gui.py" line="79"/>
+        <source>&lt;b&gt;Checker&lt;/b&gt;&lt;br /&gt;&lt;br /&gt;
+            Version: %s&lt;br /&gt;
+            %s&lt;br /&gt;&lt;br /&gt;
+            Project: &lt;a href=&quot;%s&quot;&gt;1dot75cm/repo-checker&lt;/a&gt;&lt;br /&gt;
+            License: %s&lt;br /&gt;
+            Author: &lt;a href=&quot;mailto:%s&quot;&gt;%s&lt;/a&gt;</source>
+        <translation>&lt;b&gt;Checker&lt;/b&gt;&lt;br /&gt;&lt;br /&gt;
+            版本: %s&lt;br /&gt;
+            %s&lt;br /&gt;&lt;br /&gt;
+            项目: &lt;a href=&quot;%s&quot;&gt;1dot75cm/repo-checker&lt;/a&gt;&lt;br /&gt;
+            协议: %s&lt;br /&gt;
+            作者: &lt;a href=&quot;mailto:%s&quot;&gt;%s&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="23"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="42"/>
+        <source>Basic</source>
+        <translation>基本</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="48"/>
+        <source>worker number</source>
+        <translation>工作线程数</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="49"/>
+        <source>number of worker for crawler</source>
+        <translation>爬虫的工作线程数</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="59"/>
+        <source>retry times</source>
+        <translation>重试次数</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="60"/>
+        <source>retry times if network fails</source>
+        <translation>网络失败时的重试次数</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="68"/>
+        <source>retry wait time</source>
+        <translation>重试等待时间</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="69"/>
+        <source>wait time before retry network access</source>
+        <translation>重新发起网络请求前的等待时间</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="78"/>
+        <source>Network</source>
+        <translation>网络</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="85"/>
+        <source>proxy address</source>
+        <translation>代理地址</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="91"/>
+        <source>[user:pass@]hostname[:port]</source>
+        <translation>[用户名:密码@]主机名[:端口]</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="98"/>
+        <source>http</source>
+        <translation>http</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="102"/>
+        <source>https</source>
+        <translation>https</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="106"/>
+        <source>socks4</source>
+        <translation>socks4</translation>
+    </message>
+    <message>
+        <location filename="../checker/frontends/settings.py" line="110"/>
+        <source>socks5</source>
+        <translation>socks5</translation>
     </message>
 </context>
 </TS>
